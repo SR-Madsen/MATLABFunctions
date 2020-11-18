693f2d29f99c2c9fa3a973cbc892c1f91b6bb28d
SeMad17 <semad17@student.sdu.dk>
2020-11-18 14:18:53 +0100

    Finished function for calculating temperature inaccuracy in NTC.

--- 7. sem/NTCAcc.m
+++ 7. sem/NTCAcc.m
@@ -35,23 +35,28 @@ for i = 1:length(NTCTemp)
     end
 end
 
-NTCInacc % In degrees
+disp('Inaccuracy due to NTC:')
+NTCInacc(3:end-5) % In degrees
 
 %% Inaccuracy due to equation
 EquationInacc = 1:length(NTCTemp);
 for i = 1:length(NTCTemp)
     if NTCTemp(i) < 40
         EquationInacc(i) = (19.8 * OutputVoltage(i) - 52.3) - NTCTemp(i);
+        %EquationInacc(i) = (-35 + 6.11 * OutputVoltage(i) + 2.46 * OutputVoltage(i)^2) - NTCTemp(i);
     else
-        %EquationInacc(i) = (2728 - 1261 * OutputVoltage(i) + 148 * OutputVoltage(i)^2) - NTCTemp(i);
-        EquationInacc(i) = (-29104 + 19644 * OutputVoltage(i) - 4424 * OutputVoltage(i)^2 + 333 * OutputVoltage(i)^3) - NTCTemp(i);
+        EquationInacc(i) = (2728 - 1261 * OutputVoltage(i) + 148 * OutputVoltage(i)^2) - NTCTemp(i);
+        %EquationInacc(i) = (-29104 + 19644 * OutputVoltage(i) - 4424 * OutputVoltage(i)^2 + 333 * OutputVoltage(i)^3) - NTCTemp(i);
     end
 end
 
-EquationInacc % In degrees
+disp('Inaccuracy due to equation:')
+EquationInacc(3:end-5) % In degrees
 
 %% Inaccuracy due to ADC
-ADCConvInacc = 11 * 5/(2^12); % 11 LSB inaccuracy
+ADCLSBAcc = 11; % 11 LSB inaccuracy
+ADCBit = 12; % 12-bit accuracy
+ADCConvInacc = ADCLSBAcc * SupplyVoltage/(2^ADCBit);
 
 ADCMeasVolt = OutputVoltage + ADCConvInacc;
 
@@ -66,12 +71,13 @@ for i = 1:length(NTCTemp)
     end
 end
 
-ADCInacc % In degrees
+disp('Inaccuracy due to ADC:')
+ADCInacc(3:end-5) % In degrees
 
 %% Inaccuracy due to resistor tolerance
 ResTol = 1; % [%]
 
-WorstOutputVoltage = SupplyVoltage * (SeriesRes * (1 + ResTol/100)) ./ (SeriesRes + NTCResistance);
+WorstOutputVoltage = SupplyVoltage * (SeriesRes * (1 + ResTol/100)) ./ (SeriesRes * (1 + ResTol/100) + NTCResistance);
 
 ResInacc = 1:length(NTCTemp);
 for i = 1:length(NTCTemp)
@@ -84,9 +90,15 @@ for i = 1:length(NTCTemp)
     end
 end
 
-ResInacc % In degrees
+disp('Inaccuracy due to resistor tolerance:')
+ResInacc(3:end-5) % In degrees
 
 %% Accumulated inaccuracy
-TotalInacc = abs(NTCInacc) + abs(EquationInacc) + abs(ADCInacc) + abs(ResInacc)
+TotalInacc = abs(NTCInacc(3:end-5)) + abs(EquationInacc(3:end-5)) + abs(ADCInacc(3:end-5)) + abs(ResInacc(3:end-5))
+%TotalInaccNonAbs = NTCInacc(3:end-5) + EquationInacc(3:end-5) + ADCInacc(3:end-5) + ResInacc(3:end-5)
 
-plot(NTCTemp(4:end-5), TotalInacc(4:end-5))
+plot(NTCTemp(3:end-5), TotalInacc(1:end), 'LineWidth', 2)
+grid on
+title('Total temperature variance due to all inaccuracies', 'FontSize', 20)
+xlabel('Temperature [°C]', 'FontSize', 16)
+ylabel('Inaccuracy [°C]', 'FontSize', 16)
