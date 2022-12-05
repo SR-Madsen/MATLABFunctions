1abbf3367833356a5685c39174b6a047b799abaf
SR-Madsen <sebastianrudmadsen@gmail.com>
2022-12-05 14:16:22 +0100

    Added Imperix Simulink model for code generation.

--- Speciale 11. sem/TTypeInverterVals.m
+++ Speciale 11. sem/TTypeInverterVals.m
@@ -94,6 +94,7 @@ Lc_nonlin_phi = load("nonlinear_inductor_phi.mat").phi_nonlinear;
 %% Controllers (see ControllerDesign.m)
 % Sampling frequency and delay
 fs = 4*f_sw; % [Hz]
+fs = 96006.141; % [Hz] For Imperix simulation
 Ts = 1/fs; % [s]
 Td = 2*Ts; % [s] Unused in simulation as ZOH and SimScape inductor account for it
 Tcomp = T_sw/8; % [s] Estimate of computation and propagation time
